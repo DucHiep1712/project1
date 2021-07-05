08f962e71722bf33ffdd84368e56941e1befce1a
Hiep <duchiep17122002@gmail.com>
2021-07-05 21:16:34 +0700

    Lam bai java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,34 @@
-package com.company;
+//package com.company;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World!");
+	    Scanner scanner = new Scanner(System.in);
+	    List<String> sinhVien = new ArrayList<String>();
+	    List<String> giangVien = new ArrayList<String>();
+	    int n, m;
+	    System.out.println("Số sinh viên là: ");
+	    n = scanner.nextInt();
+	    for (int i = 0; i < n; i++) {
+            sinhVien.add(scanner.next());
+        }
+	    System.out.println("\n" + "Số giảng viên là: ");
+	    m = scanner.nextInt();
+	    for (int i = 0; i < m; i++) {
+            giangVien.add(scanner.next());
+        }
+        System.out.println("Các sinh viên là: \n");
+	    for (int i = 0; i < n; i++) {
+	    	int temp = i + 1;
+            System.out.println(temp + "." + sinhVien.get(i) + "\n");
+        }
+		System.out.println("Các giảng viên là: \n");
+	    for (int i = 0; i < m; i++) {
+	    	int temp = i + 1;
+			System.out.println(temp + "." + giangVien.get(i) + "\n");
+		}
     }
 }
