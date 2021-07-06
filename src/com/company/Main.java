8bbc2a43c687de31c40297345baf948a5f1f1e9e
Hiep <duchiep17122002@gmail.com>
2021-07-06 12:03:43 +0700

    Modified Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,83 @@
-//package com.company;
+package com.company;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
 public class Main {
 
+	public static class Stdin {
+		private final String name, id, classId;
+
+		public Stdin(String name, String id, String classId) {
+			this.name = name;
+			this.id = id;
+			this.classId = classId;
+		}
+	}
+
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
-	    List<String> sinhVien = new ArrayList<String>();
-	    List<String> giangVien = new ArrayList<String>();
+	    List<Stdin> sinhVien = new ArrayList<>();
+	    List<String> giangVien = new ArrayList<>();
 	    int n, m;
 	    System.out.println("Số sinh viên là: ");
 	    n = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Hãy nhập thông tin các sinh viên");
 	    for (int i = 0; i < n; i++) {
-            sinhVien.add(scanner.next());
+	    	int j = i + 1;
+	    	String tName, tId, tClassId;
+			System.out.println("Nhập tên sinh viên " + j + ": ");
+            tName = scanner.nextLine();
+			System.out.println("Nhập mã số sinh viên " + j + ": ");
+			tId = scanner.nextLine();
+			System.out.println("Nhập tên lớp của sinh viên " + j + ": ");
+			tClassId = scanner.nextLine();
+			Stdin temp = new Stdin(tName, tId, tClassId);
+			sinhVien.add(temp);
         }
-	    System.out.println("\n" + "Số giảng viên là: ");
+	    for (int i = 0; i < n; i++) {
+			System.out.println(sinhVien.get(i).name + ", " + sinhVien.get(i).id + ", " + sinhVien.get(i).classId + "\n");
+		}
+	    System.out.println("Số giảng viên là: ");
 	    m = scanner.nextInt();
+	    scanner.nextLine();
+		System.out.println("Hãy nhập tên các giảng viên");
 	    for (int i = 0; i < m; i++) {
-            giangVien.add(scanner.next());
+	    	int j = i + 1;
+			System.out.println("Giảng viên " + j + ": ");
+            giangVien.add(scanner.nextLine());
         }
-        System.out.println("Các sinh viên là: \n");
-	    for (int i = 0; i < n; i++) {
-	    	int temp = i + 1;
-            System.out.println(temp + "." + sinhVien.get(i) + "\n");
-        }
-		System.out.println("Các giảng viên là: \n");
-	    for (int i = 0; i < m; i++) {
-	    	int temp = i + 1;
-			System.out.println(temp + "." + giangVien.get(i) + "\n");
+        while (true) {
+			System.out.println("Nhấn 1 nếu bạn muốn tìm thông tin sinh viên, 2 để biết các giảng viên hoặc -1 để kết thúc chương trình!");
+			//clearScreen();
+			int opt = scanner.nextInt();
+			if (opt == 1) {
+				boolean check = false;
+				scanner.nextLine();
+				System.out.println("Nhập mã số sinh viên hoặc tên sinh viên: ");
+				String inf = scanner.nextLine();
+				for (int i = 0; i < n; i++) {
+					if (sinhVien.get(i).name.equals(inf) || sinhVien.get(i).id.equals(inf)) {
+						check = true;
+						System.out.println("Tên sinh viên: " + sinhVien.get(i).name + "\n");
+						System.out.println("Mã số sinh viên: " + sinhVien.get(i).id + "\n");
+						System.out.println("Mã lớp sinh viên: " + sinhVien.get(i).classId);
+						break;
+					}
+				}
+				if (!check) System.out.println("Sinh viên không tồn tại!");
+			}
+			else if (opt == 2) {
+				for (int i = 0; i < m; i++) {
+					int j = i + 1;
+					System.out.println("Giảng viên " + j + " : " + giangVien.get(i) + "\n");
+				}
+			}
+			else if (opt == -1) return;
+			else {
+				System.out.println("Hãy nhập lại lựa chọn của bạn!");
+			}
 		}
     }
 }
